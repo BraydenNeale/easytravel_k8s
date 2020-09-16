db89f4898069452503f34eb578eabb4598682279
Brayden Neale <brayden.neale@dynatrace.com>
2020-09-16 18:17:27 +1000

    move config to a config map, disable probes (debugging), don't launch loadgen by default

--- create_easytravel.sh
+++ create_easytravel.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 kubectl create namespace easytravel
-#kubectl apply -f manifests/easytravel-mongodb-secret.yaml
+kubectl apply -f manifests/easytravel-mongodb-secret.yaml
+kubectl apply -f manifests/easytravel-config.yaml
 kubectl apply -f manifests/easytravel-mongodb.yaml
 sleep 5
 
 kubectl apply -f manifests/easytravel-backend.yaml
 kubectl apply -f manifests/easytravel-frontend.yaml
 kubectl apply -f manifests/easytravel-nginx.yaml
-sleep 30
 
-kubectl apply -f manifests/easytravel-loadgen.yaml
+#kubectl apply -f manifests/easytravel-loadgen.yaml
--- stop_easytravel.sh
+++ stop_easytravel.sh
@@ -2,4 +2,4 @@ kubectl -n easytravel scale --replicas=0 deployment/loadgen
 kubectl -n easytravel scale --replicas=0 deployment/easytravel-www
 kubectl -n easytravel scale --replicas=0 deployment/easytravel-frontend
 kubectl -n easytravel scale --replicas=0 deployment/easytravel-backend
-kubectl -n easytravel scale --replicas=0 deployment/easytravel-mongodb
+#kubectl -n easytravel scale --replicas=0 deployment/easytravel-mongodb
