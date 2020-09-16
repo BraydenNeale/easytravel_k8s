c57a256924fcd544b56e9fe6392ebcc578c20ce6
Brayden Neale <brayden.neale@dynatrace.com>
2020-09-16 16:09:09 +1000

    Initial commit: manifest files and setup scripts

---
+++ stop_easytravel.sh
@@ -0,0 +1,5 @@
+kubectl -n easytravel scale --replicas=0 deployment/loadgen
+kubectl -n easytravel scale --replicas=0 deployment/easytravel-www
+kubectl -n easytravel scale --replicas=0 deployment/easytravel-frontend
+kubectl -n easytravel scale --replicas=0 deployment/easytravel-backend
+kubectl -n easytravel scale --replicas=0 deployment/easytravel-mongodb
